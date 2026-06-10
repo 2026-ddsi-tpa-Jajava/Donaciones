6e1d1dba85b80e90b04ff70f4babdd990bc545f8
Alejo Menay <amenay@frba.utn.edu.ar>
2026-06-10 17:10:00 -0300

    arreglo

--- src/main/java/ar/edu/utn/dds/k3003/Fachada.java
+++ src/main/java/ar/edu/utn/dds/k3003/Fachada.java
@@ -12,7 +12,7 @@ import ar.edu.utn.dds.k3003.mappers.IdentificadoresDataMapper;
 import ar.edu.utn.dds.k3003.service.DonacionesService;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import ar.edu.utn.dds.k3003.catedra.dtos.donaciones.*;
 import ar.edu.utn.dds.k3003.catedra.dtos.donadoresYEntidades.QuejaDTO;
@@ -23,7 +23,7 @@ import ar.edu.utn.dds.k3003.mappers.DonacionesDataMapper;
 
 import java.time.LocalDate;
 
-@Service
+@Component
 public class Fachada implements FachadaDonaciones{
 
     private final DonacionesService donacionesService;
